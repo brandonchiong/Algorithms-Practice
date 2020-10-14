8f13ba54de57c59bbdecb6eea9c2405c08fccc79
Brandon Chiong <brandonchiong@gmail.com>
2020-10-13 20:29:20 -0700

    Update mergesort.py

--- Python/mergesort.py
+++ Python/mergesort.py
@@ -24,3 +24,16 @@ def merge_sorted_lists(list1, list2):
             index1 += 1
 
     return result
+
+def mergesort(input_list):
+    if len(input_list) <= 1:
+        return input_list
+
+    middle = int(len(input_list) / 2)
+    list1 = input_list[:middle]
+    list2 = input_list[middle:]
+
+    left = mergesort(list1)
+    right = mergesort(list2)
+
+    return merge_sorted_lists(left, right)
